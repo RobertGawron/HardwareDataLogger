e3d352041f8b631a5137a9d48519278cbcb3e985
Robert Gawron <robert_gawron@yahoo.fr>
2020-10-17 12:11:12 +0200

    fixes due to probems found during unit tests

--- Software/NUCLEO-F091RC/Src/gm_histogram.c
+++ Software/NUCLEO-F091RC/Src/gm_histogram.c
@@ -2,6 +2,7 @@
 #include "gm_circular_buffer.h"
 #include "gm_measurement.h"
 
+// binIndex is zero-base numbered
 uint16_t GMHistogram_GetBinValue(uint8_t binIndex, uint8_t totalBinAmount)
 {
     uint16_t binValue = 0u;
@@ -11,17 +12,27 @@ uint16_t GMHistogram_GetBinValue(uint8_t binIndex, uint8_t totalBinAmount)
     GMCircularBuffer_GetMinMaxElement(&minElement, &maxElement);
     // TODO check return value
 
-    GMMeasurement_Value_t binLength = (maxElement - minElement) / totalBinAmount;
+    // TODO +1 is to avoid overlap, I don't know how it works
+    GMMeasurement_Value_t binLength = (maxElement - minElement) / totalBinAmount + 1; 
 
-    GMMeasurement_Value_t elementOfBinMinValue = binLength * binIndex;
+    GMMeasurement_Value_t elementOfBinMinValue = minElement + binLength * binIndex;
     GMMeasurement_Value_t elementOfBinMaxValue = elementOfBinMinValue + binLength;
-printf("%d\n", maxElement);
+
+#if DEBUG_PRINTS_ENABLED
+    printf("\tminElement: %d \n\tmaxElement: %d \n\tbinLength %d \n\telementOfBinMinValue: %d \n\telementOfBinMaxValue %d\n\n" 
+        , minElement
+        , maxElement
+        , binLength 
+        , elementOfBinMinValue 
+        , elementOfBinMaxValue);
+#endif
+
     for(uint16_t j = 0u; j < GMCircularBuffer_GetElementCount(); j++)
     {
         GMMeasurement_Value_t sampleValue; 
         GMCircularBuffer_GetElement(&sampleValue, j);
-        
-        if ((sampleValue >= elementOfBinMinValue) && (sampleValue < elementOfBinMaxValue))
+
+        if ((sampleValue >= elementOfBinMinValue) && (sampleValue <= elementOfBinMaxValue))
         {
             binValue++;
         }
