ca386471fef99b389320b7e29503e725895a1606
Robert <robert_gawron@yahoo.fr>
2021-08-25 22:35:13 +0200

    Feature/rev 2 0 (#47) (#48)
    * vanilla configuration
    
    * porting mngm of keyabord from old version of the project (designed only
    for GM counter)
    
    * added st7735s, it compiles ok, but is neither used nor its callback are implemented; updated docs
    
    * fix img url, add info that NUCLEO-F091RC firmware is obsolete

---
+++ Software/NUCLEO-F103RB/Inc/logger_keyboard_key_state.h
@@ -0,0 +1,10 @@
+#ifndef LOGGER_KEYBOARD_KEY_STATE_H
+#define LOGGER_KEYBOARD_KEY_STATE_H
+
+typedef enum LoggerKeyboardKeys_Key_State_t
+{
+    LOGGER_KEYBOARD_KEY_SET = 0u,
+	LOGGER_KEYBOARD_KEY_NOT_SET = 1u
+} LoggerKeyboardKeys_Key_State_t;
+
+#endif
---
+++ Software/NUCLEO-F103RB/Src/logger_keyboard.c
@@ -0,0 +1,20 @@
+#include "logger_keyboard.h"
+//#include "gm_display_updater.h"
+
+void LoggerKeyboard_Init()
+{
+
+}
+
+
+void LoggerKeyboard_OnKeyPressDetection(
+		LoggerKeyboardKeys_Key_State_t keyUp,
+		LoggerKeyboardKeys_Key_State_t keyDown,
+		LoggerKeyboardKeys_Key_State_t keyLeft,
+		LoggerKeyboardKeys_Key_State_t keyRight)
+{
+    //GMDisplayUpdater_ReactOnKeyPress(key);
+	// for debug to check if it is executed
+	volatile int x = 0;
+	x++;
+}
